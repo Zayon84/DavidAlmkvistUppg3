e1db64f882ca766fd00fd86f65ab4930a8083128
Zayon84 <dalmkvist@hotmail.com>
2023-02-23 10:02:51 +0100

    Add project files.

---
+++ DavidAlmkvistUppg3/Shared/Company.cs
@@ -0,0 +1,21 @@
+﻿
+namespace DavidAlmkvistUppg3.Shared;
+
+public class Company
+{
+	public string CompanyName { get; set; }
+	public bool Applied { get; set; }
+	public string Comment { get; set; }
+	public String Description { get; set; }
+	public string Location { get; set; }
+
+	public Company(string name, bool applied, string comment , string description, string location)
+	{
+		CompanyName = name;
+		Applied = applied;
+		Comment = comment;
+		Description = description;
+		Location = location;
+	}
+}
+
---
+++ DavidAlmkvistUppg3/Shared/LiaStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace DavidAlmkvistUppg3.Shared;
+
+public class LiaStatus
+{
+	public Guid CompanyID { get; set; }
+	public bool Applied { get; set; }
+	public DateTime AppliedDate { get; set; }
+	public string Comment { get; set; }
+
+	public LiaStatus(Guid companyId, bool applied, DateTime appliedDate, string comment)
+	{
+		CompanyID = companyId;
+		Applied = applied;
+		AppliedDate = appliedDate;
+		Comment = comment;
+	}
+}
