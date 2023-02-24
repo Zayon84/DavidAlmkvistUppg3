81620ee2361c939e727e36594362f3b9fea62373
Zayon84 <dalmkvist@hotmail.com>
2023-02-24 05:44:23 +0100

    Added Repository -Add -GetAll

---
+++ DavidAlmkvistUppg3/Server/DataAccess/CompanyRepository.cs
@@ -0,0 +1,34 @@
+﻿using DavidAlmkvistUppg3.Server.DataAccess.Models;
+using DavidAlmkvistUppg3.Shared;
+
+namespace DavidAlmkvistUppg3.Server.DataAccess
+{
+	public class CompanyRepository
+	{
+		private readonly CompanyContext _context;
+
+		public CompanyRepository(CompanyContext context)
+		{
+			_context = context;
+		}
+
+		public void Add(Company company)
+		{
+			var newCompanyModel = new CompanyModel();
+
+			newCompanyModel.CompanyName = company.CompanyName;
+			newCompanyModel.Location = company.Location;
+			newCompanyModel.Description = company.Description;
+			newCompanyModel.Applied = company.Applied;
+			newCompanyModel.Comment = company.Comment;
+
+			_context.Company.Add(newCompanyModel);
+			_context.SaveChanges();
+		}
+
+		public IEnumerable<Company> GetAll()
+		{
+			return _context.Company.Select(cm => new Company(cm.CompanyName, cm.Location, cm.Description, cm.Applied, cm.Comment));
+		}
+	}
+}
